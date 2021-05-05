f128c54c3e2d8a0e4c923f0024b63cc8b6522c0f
DonDrews <donovancarldrews@gmail.com>
2021-05-04 21:30:33 -0700

    Copied existing implementation of uart tx and rx components. Created testbench for evaluating.

---
+++ deon_fpga.srcs/constrs_1/new/constr.xdc
@@ -0,0 +1,5 @@
+set_property PACKAGE_PIN W5 [get_ports clk] 
+set_property IOSTANDARD LVCMOS33 [get_ports clk] 
+set_property PACKAGE_PIN T18 [get_ports rst_n]
+set_property IOSTANDARD LVCMOS33 [get_ports rst_n]
+create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports clk]
---
+++ deon_fpga.srcs/sources_1/new/transmit_test.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/04/2021 06:07:00 PM
+// Design Name: 
+// Module Name: transmit_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module transmit_test;
+    
+    always begin
+        clk = 0;
+        #(5) clk = 1;
+        #(5);
+    end
+    
+    initial begin
+        rst_n = 0;
+        #(10);
+        rst_n = 1;
+    end
+    
+    reg clk;
+    reg rst_n;
+    wire transmit;
+    wire data_in;
+    reg[7:0] data;
+    uart_transmit tx(
+        .clk(clk),
+        .rst_n(rst_n),
+        .transmit(transmit),
+        .data(data),
+        .TxD(TxD)
+    );
+    
+    uart_receive rx(
+        .clk(clk),
+        .rst_n(rst_n),
+        .RxD(TxD),
+        .has_output(data_in)
+    );
+    
+    reg[8:0] countdown;
+    always @(posedge clk) begin
+        if(~rst_n) begin
+            countdown <= 0;
+            data <= 8'b10101010;
+        end else begin
+            countdown <= countdown + 9'b1;
+            if(countdown == 1)
+            begin
+                data <= data + 1;
+            end
+        end
+    end
+    
+    assign transmit = (countdown > 1 && countdown < 100);
+    
+endmodule
